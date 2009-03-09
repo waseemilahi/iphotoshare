313d6a45a21dd2646bb250bd883ed96d319032c6
waseemilahi <waseemilahi@1fea8144-fc27-11dd-a295-0b7b0935bf94>
2009-03-09 03:36:59 +0000

    Use This Version: The older Version was Messed Up.

---
+++ trunk/PhotoShare/Classes/CameraViewController.h
@@ -0,0 +1,16 @@
+//
+//  CameraViewController.h
+//  PhotoShare
+//
+//  Created by Waseem Ilahi on 3/8/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CameraViewController : UIViewController {
+
+}
+
+@end
