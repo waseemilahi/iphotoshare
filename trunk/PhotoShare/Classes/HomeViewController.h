fec289e1c8a7f68c701ebad72178072b22b5c78c
waseemilahi <waseemilahi@1fea8144-fc27-11dd-a295-0b7b0935bf94>
2009-03-09 22:21:25 +0000

    Register Button Works :)

---
+++ trunk/PhotoShare/Classes/HomeViewController.h
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.h
+//  PhotoShare
+//
+//  Created by Waseem Ilahi on 3/8/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HomeViewController : UIViewController {
+	
+	//IBOutlet id displayedField;	
+
+}
+
+//- (IBAction) homeSignIn:(id)sender;
+
+@end
