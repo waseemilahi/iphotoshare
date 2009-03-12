561c401922c2f62b0ec7103420d80e2063f80239
waseemilahi <waseemilahi@1fea8144-fc27-11dd-a295-0b7b0935bf94>
2009-03-12 04:21:45 +0000

    (Another Update)

---
+++ trunk/PhotoShare/Classes/SettingsViewController.h
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.h
+//  PhotoShare
+//
+//  Created by Waseem Ilahi on 3/8/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+
+	IBOutlet id displayedField;
+}
+
+- (IBAction) signIn:(id)sender;
+- (IBAction) homeSignIn:(id)sender;
+- (IBAction) Register:(id)sender;
+
+@end
