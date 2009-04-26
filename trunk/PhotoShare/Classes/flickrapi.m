b0ca6d6b1af694703525c60bf50287e5ff0b46fc
waseemilahi <waseemilahi@1fea8144-fc27-11dd-a295-0b7b0935bf94>
2009-04-26 01:07:44 +0000

    Reverted some thngs to get the user input for photo name working again

--- trunk/PhotoShare/Classes/flickrapi.m
+++ trunk/PhotoShare/Classes/flickrapi.m
@@ -461,7 +461,7 @@
 }
 
 
--(BOOL)uploadPhoto:(UIImage *)image withLat:(NSInteger)lat andLon:(NSInteger)lon {
+-(BOOL)uploadPhoto:(UIImage *)image withLat:(NSInteger)lat andLon:(NSInteger)lon withName:(NSString *)imageName{
 	[self clearParams];
 	[self addParam:@"api_key" withValue:APIKEY];
 	[self addParam:@"auth_token" withValue:TOKEN];
